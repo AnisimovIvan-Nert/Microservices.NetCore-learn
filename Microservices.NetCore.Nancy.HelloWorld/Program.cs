78a1b36115d4edf0ed8fd77a675d8277eb7c6f7b
Ivan Ansimov <anisimov.ivan.work@gmail.com>
2025-07-14 16:03:25 +0300

    Nancy hello world middleware

--- Microservices.NetCore.Nancy.HelloWorld/Program.cs
+++ Microservices.NetCore.Nancy.HelloWorld/Program.cs
@@ -5,7 +5,17 @@ var builder = WebApplication.CreateBuilder(args);
 builder.AllowSynchronousIO();
 
 var app = builder.Build();
-app.UseOwin(pipeline => pipeline.UseNancy());
+
+app.UseOwin(pipeline =>
+{
+    pipeline(next => environment =>
+    {
+        Console.WriteLine("Got request");
+        return next(environment);
+    });
+    pipeline.UseNancy();
+});
+
 app.Run();
 
 
